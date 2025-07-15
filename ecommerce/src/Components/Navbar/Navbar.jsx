df99c01a723eba66bdb26c025c001542d2cd01b2
sachin Gupta <sachinguptasg7633780@gmail.com>
2025-07-15 19:11:49 +0530

    Added the functionality of cart totalItems

--- ecommerce/src/Components/Navbar/Navbar.jsx
+++ ecommerce/src/Components/Navbar/Navbar.jsx
@@ -1,11 +1,14 @@
 import "./Navbar.css";
 import logo from "../../assets/logo.webp";
 import cart from "../../assets/Cart.webp";
-import { useState } from "react";
+import { useContext, useState } from "react";
 import { useLocation } from "react-router-dom";
 import { Link } from "react-router-dom";
+import { ShopContext } from "../../Context/ShopContext";
+
 function Navbar() {
   const [menu, setMenu] = useState("home");
+  const {getTotalCartItems} = useContext(ShopContext);  
 
   const location = useLocation();
   const path = location.pathname;
@@ -58,7 +61,7 @@ function Navbar() {
           <Link to="/cart">
             <img src={cart} alt="" height="40px" />
           </Link>
-          <div className="nav-cart-count">0</div>
+          <div className="nav-cart-count">{getTotalCartItems()}</div>
         </div>
       </div>
     </>
